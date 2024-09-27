7a04ea0b753cd88c8252cb1100081b8b8749df6d
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2024-09-27 17:38:34 +0300

    Fix: class extension bug

--- compose-metric-reporter/src/main/kotlin/com/hamurcuabi/compose/metric/reporter/model/details/DetailedStatistics.kt
+++ compose-metric-reporter/src/main/kotlin/com/hamurcuabi/compose/metric/reporter/model/details/DetailedStatistics.kt
@@ -1,6 +1,6 @@
 package com.hamurcuabi.compose.metric.reporter.model.details
 
 data class DetailedStatistics(val items: List<DetailRowItems>) {
-    val headers: List<String> get() = items.first().detailItem.map { DetailItem.name }
+    val headers: List<String> get() = items.first().detailItem.map { it.name }
 }
 
